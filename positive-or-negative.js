03aa2a494142bea3caaf75e74e1c128b2682b087
Ali-Ahmed-Khan <IIaliahmedkhanII@gmail.com>
2018-11-07 01:15:37 +0530

    Added Algorithm 2

---
+++ positive-or-negative.js
@@ -0,0 +1,12 @@
+// Algorithm 2 - Positive or Negative number
+
+function pos_to_negative(num) {
+    if (num >= 0) {
+        console.log('The number you entered is a positive number');
+    }
+    else {
+        console.log('The number you entered is a negative number');
+    }    
+}
+
+pos_to_negative(-12);
