2be9a045b07c72e2548a29bc5a2eb57fc4e0a498
Ali-Ahmed-Khan <IIaliahmedkhanII@gmail.com>
2018-11-07 01:13:44 +0530

    Added Algorithm 1

---
+++ odd-or-even.js
@@ -0,0 +1,12 @@
+// Algorithm 1 - Odd or Even
+
+function even(input) {
+    if (input % 2 == 0) {
+        console.log("Entered number is an even number")
+    }
+    else {
+        console.log('Entered number is an odd number')
+    }
+}
+
+even(5);
